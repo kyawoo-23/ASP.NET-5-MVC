d1b7b09245b72b9a2ac2d1025c2b1d6871cb9084
Kyaw Kyaw Oo <kyawkyawjek@gmail.com>
2023-03-22 09:55:11 +0630

    Add project files.

---
+++ ModelBindingDemo/Controllers/DeveloperController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBindingDemo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModelBindingDemo.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        private readonly static List<Developer> _developers = new()
+        {
+            new Developer {
+                Id = 1,
+                Name = "John",
+                Gender = "Male",
+                Type = "Frontend developer"
+            },
+            new Developer {
+                Id = 2,
+                Name = "Eric",
+                Gender = "Male",
+                Type = "Backend developer"
+            },
+        };
+
+        public IActionResult Index()
+        {
+            return View(_developers);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Developer developer)
+        {
+            developer.Id = _developers.Count + 1;
+            _developers.Add(developer);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Developer data = _developers.FirstOrDefault(x => x.Id == id);
+            return data != null ? View(data) : RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Developer developer)
+        {
+            Developer data = _developers.FirstOrDefault(x => x.Id == developer.Id);
+            data.Name = developer.Name;
+            data.Gender = developer.Gender;
+            data.Type = developer.Type;
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Developer data = _developers.FirstOrDefault(x => x.Id == id);
+            if (data != null)
+            {
+                _developers.Remove(data);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            
+        }
+    }
+}
---
+++ ModelBindingDemo/Models/Developer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ModelBindingDemo.Models
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Type { get; set; }
+    }
+}
